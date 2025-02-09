9eaa1ce267c5be4adf5ec68eb0a507e3edf46da6
John Doe <johndoe@example.com>
2025-02-09 14:40:15 -0700

    initial grammar and example file

--- grammar.js
+++ grammar.js
@@ -7,11 +7,25 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+const newline = /[\r\n\u2028\u2029]/;
+const nonNewline = /[^\r\n\u2028\u2029]/;
+
 module.exports = grammar({
   name: "oats",
-
+  extras: $ => [$.comment, ' '],
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat(choice($.instruction_line, $.breaker, $.clipboard_line, $.comment)),
+    breaker: $ => newline,
+    instruction_line: $ => seq($.marker, $.instruction, newline),
+    instruction: $ => repeat1(nonNewline),
+    marker: $ => choice($.unordered, $.one_of, $.and_then, $.optional),
+    unordered: $ => '&',
+    one_of: $ => '|',
+    and_then: $ => '~',
+    optional: $ => '?',
+    clipboard_line: $ => seq($.clipbo, $.clipboard_string, newline),
+    clipbo: $ => token.immediate('= '),
+    clipboard_string: $ => token.immediate(nonNewline),
+    comment: _ => token(seq('//', repeat1(nonNewline))),
   }
 });
