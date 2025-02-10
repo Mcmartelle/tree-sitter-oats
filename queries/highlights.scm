c2a601dadae42d40d20a77cff5540c476665736f
John Doe <johndoe@example.com>
2025-02-09 22:53:25 -0700

    improved highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,7 +1,17 @@
-(marker) @operator
+(instruction) @variable
+
+[
+  (unordered)
+  (and_then)
+] @keyword.storage.type
+
+[
+  (optional)
+  (one_of)
+] @keyword.control.conditional
 
 (clipbo) @punctuation.delimiter
 
-(clipboard_string) @string
+(clipboard_string) @string.special
 
 (comment) @comment
